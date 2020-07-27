4879188538c144690c573caf79948a31808cf614
Ganesh Agrawal <gmegotiya@gmail.com>
2020-07-27 19:02:39 +0530

    temp

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,6 +1,8 @@
 export default (req, res) => {
+    const SERVER = process.env.DB_SERVER;
     res.status(404).json({
         error: "You lost here... 💩",
-        status: 404
+        status: 404,
+        SERVER
     })
 }
